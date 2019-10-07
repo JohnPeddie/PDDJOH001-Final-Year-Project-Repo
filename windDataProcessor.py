ccb6e832470dee1a3b2b5054e454cd141817f717
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2019-10-07 11:45:12 +0200

    Added average functionality + script clean up

--- windDataProcessor.py
+++ windDataProcessor.py
@@ -1,5 +1,9 @@
 import csv
 from collections import Counter
+from statistics import mean
+
+def remove_values_from_list(the_list, val):
+    return [value for value in the_list if value != val]
 
 def frange(start, stop=None, step=None):
     if stop == None:
@@ -21,7 +25,7 @@ def frequincyPlotValuesForExcel(dataList):
     roundedList = []
     for i in dataList:
         roundedList.append(round(float(i),1))
-        
+    print("mean: ", mean(roundedList))    
     counted = Counter(roundedList)
     print("Rounded values:")
     for k in sorted(counted):
@@ -29,6 +33,13 @@ def frequincyPlotValuesForExcel(dataList):
     print("Frequency:")
     for l in sorted(counted):
         print(counted[l])
+        
+def averageSpeed(data):
+    floatList = []
+    for i in data:
+        floatList.append(float(i))
+    return mean(floatList)
+        
         
 def frequincyPlotValuesForExcelAll(dataArray):
     counts =[]
@@ -61,25 +72,33 @@ def roundedTest(dataList, title):
         print(q)
 
 naming = ['01','02','03','04','05','06','07','08','09','10','11','12']
+ws62 = []
+ws60 = []
+ws40 = []
+ws20 = []
+ws10 = []
 for i in naming:  
     with open ('wm08_2018'+i+'_v01.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        ws62 = []
-        ws60 = []
-        ws40 = []
-        ws20 = []
-        ws10 = []
+       
         for line in csv_reader:
-            if ((len(line)>10) and (line[0]!="date_time")):#ignore legend and headings
+            if ((len(line)>10) and (line[0]!="date_time")): #ignore legend and headings
                 ws62.append(line[2])
                 ws60.append(line[6])
                 ws40.append(line[10])
                 ws20.append(line[14])
                 ws10.append(line[18])
+                
 
-#roundedTest(ws60, "ws60")
-#roundedTest(ws60, "ws60")
-#roundedTest(ws40, "ws40")
-#roundedTest(ws20, "ws20")
-#roundedTest(ws10, "ws10")
+
+ws62 = remove_values_from_list(ws62, ' NULL')
+ws60 = remove_values_from_list(ws60, ' NULL')
+ws40 = remove_values_from_list(ws40, ' NULL')
+ws20 = remove_values_from_list(ws20, ' NULL')
+ws10 = remove_values_from_list(ws10, ' NULL')
+
+
+print("Average value: ", averageSpeed(ws62+ws60+ws40+ws20+ws10))
+print("Total data points: ", (len(ws62)+len(ws60)+len(ws40)+len(ws20)+len(ws10)))
+
 frequincyPlotValuesForExcelAll([ws62,ws60,ws40,ws20,ws10])
